5950300fb66fa1d77217506e37e33b15fee29b95
Matt Austin <mpaustin13@gmail.com>
2021-03-17 21:26:39 -0400

    added about page and figured out styling issue

--- components/Header.js
+++ components/Header.js
@@ -1,26 +1,13 @@
 import React from 'react'
 import { Toolbar } from '@material-ui/core'
 import { Box } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles'
-
-const useStyles = makeStyles({
-    header: {
-        backgroundColor: '#808080',
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        margin: '5%',
-    },
-})
-
+import styles from '../styles/Home.module.css'
 
 export default function Header() {
 
-    const classes = useStyles();
-
     return (
         <Toolbar >
-            <Box className={classes.header}
+            <Box className={styles.header}
             // width='100%' display='flex' flexDirection='row' justifyContent='space-between' 
             >
                 <Box >
@@ -30,7 +17,7 @@ export default function Header() {
                 </Box>
                 <Box>
                     <h3>
-                        About
+                        <a href='/about'>About</a>
                     </h3>
                 </Box>
             </Box>
