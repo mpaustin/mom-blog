39def964457528af5ce74e6cce463c80bc56a62f
Matt Austin <mpaustin13@gmail.com>
2021-03-19 08:35:02 -0400

    fixed issue with header

--- components/Header.js
+++ components/Header.js
@@ -2,14 +2,13 @@ import React from 'react'
 import { Toolbar } from '@material-ui/core'
 import { Box } from '@material-ui/core';
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Header() {
 
     return (
         <Toolbar >
-            <Box className={styles.header}
-            // width='100%' display='flex' flexDirection='row' justifyContent='space-between' 
-            >
+            <Box className={styles.header} >
                 <Box >
                     <h2>
                         Attabuoy and Kel 
@@ -17,7 +16,12 @@ export default function Header() {
                 </Box>
                 <Box>
                     <h3>
-                        <a href='/about'>About</a>
+                        <Link href='/about'>About</Link>
+                    </h3>
+                </Box>
+                <Box>
+                    <h3>
+                        <Link href='/all-posts'>Blog</Link>
                     </h3>
                 </Box>
             </Box>
--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import Head from 'next/head'
+import Header from '../components/Header'
 
 export default function about() {
     return (
         <div>
+            <Head>
+                <Header/>
+            </Head>
             <h1>About</h1>
         </div>
     )
